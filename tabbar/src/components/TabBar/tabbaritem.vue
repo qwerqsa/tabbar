aebdf35b57950d56a8728ecd2af6d43dba940dce
贺齐 <2091287214@qq.com>
2021-10-10 11:38:04 +0800

    底部导航

---
+++ tabbar/src/components/TabBar/tabbaritem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="tab-bar-item">
+    <!-- <img src="../../assets/tabbar/cart.svg" alt=""> -->
+    <!-- 首页 -->
+    <slot></slot>
+    <slot></slot>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+ .tab-bar-item{
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+  }
+  .tab-bar-item img{
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    vertical-align: middle;
+    margin-bottom: 2px;
+  }
+</style>
